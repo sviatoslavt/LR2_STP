61a172f15d894beb968e78c1ef8907d16a7801ef
sviatoslavt <svyatoslav.tashchuk@gmail.com>
2024-03-05 01:07:36 +0200

    lr3

---
+++ app_blog/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+from .models import ArticleImage
+
+class ArticleImageForm(forms.ModelForm):
+    image = forms.ImageField(
+        widget=forms.ClearableFileInput())
+
+    class Meta:
+        model = ArticleImage
+        fields = '__all__'
