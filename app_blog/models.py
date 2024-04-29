1a17c1885f879429bd2eff9e3409339c04ba0291
sviatoslavt <svyatoslav.tashchuk@gmail.com>
2024-04-29 23:41:11 +0300

    lr 4-5

--- app_blog/models.py
+++ app_blog/models.py
@@ -10,8 +10,21 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 символів')
-    slug = models.SlugField(u'Slug')
+    category = models.CharField(u'Категорія',max_length=250, help_text=u'Максимум 250 символів')
+    slug = models.SlugField(u'Слаг')
+    objects = models.Manager()
+    class Meta:
+        verbose_name = u'Категорія для публікації'
+        verbose_name_plural = u'Категорії для публікацій'
+    def __str__(self):
+        return self.category
+
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={'slug': self.slug})
+        except:
+            url = "/"
+        return url
 
 
 class Meta:
